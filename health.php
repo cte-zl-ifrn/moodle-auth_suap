aaf34bbdbd55473403953fe850a1deb324a0ee84
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-12-05 21:09:25 -0300

    feat: [add] alguns checks extras ao health.php : client_id, authorize_url, token_url, rh_eu_url, logout_url.

--- health.php
+++ health.php
@@ -1,7 +1,15 @@
 <?php
-define("MOODLE_INTERNAL", true);
-define("MATURITY_STABLE", true);
+include_once("../../version.php");
+$conf = get_config('auth_suap');
+
 $plugin = new stdClass();
 include_once("version.php");
+echo "<pre>";
 echo "release: $plugin->release<br>";
 echo "version: $plugin->version<br>";
+echo "client_id: $conf->client_id<br>";
+echo "authorize_url: $conf->authorize_url<br>";
+echo "token_url: $conf->token_url<br>";
+echo "rh_eu_url: $conf->rh_eu_url<br>";
+echo "logout_url: $conf->logout_url<br>";
+echo "</pre>";
